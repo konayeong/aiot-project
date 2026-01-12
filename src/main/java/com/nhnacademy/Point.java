a66a2739ad40e71f4ad7d3cf2d8cbaab4602098c
konayeong <dnjfrhkd402@naver.com>
2026-01-12 10:37:36 +0900

    Point 실습

---
+++ src/main/java/com/nhnacademy/Point.java
@@ -0,0 +1,29 @@
+package com.nhnacademy;
+
+public class Point {
+    private final double x;
+    private final double y;
+
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public double distanceTo(Point other) {
+        if (other == null) {
+            throw new IllegalArgumentException("포인트가 null일 수 없음");
+        }
+        double subX = other.getX() - x;
+        double subY = other.getY() - y;
+
+        return Math.sqrt(subX * subX + subY * subY);
+    }
+}
