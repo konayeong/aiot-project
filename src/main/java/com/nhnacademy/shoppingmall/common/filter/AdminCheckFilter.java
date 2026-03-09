c28c3910a470ec2db588d8861b182db0cd719deb
konayeong <dnjfrhkd402@naver.com>
2026-03-09 16:28:13 +0900

    TODO8-11 Filter

--- src/main/java/com/nhnacademy/shoppingmall/common/filter/AdminCheckFilter.java
+++ src/main/java/com/nhnacademy/shoppingmall/common/filter/AdminCheckFilter.java
@@ -1,5 +1,7 @@
 package com.nhnacademy.shoppingmall.common.filter;
 
+import com.nhnacademy.shoppingmall.user.domain.User;
+import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
 
 import jakarta.servlet.*;
@@ -10,9 +12,21 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @Slf4j
+@WebFilter(
+        filterName = "adminCheckFilter",
+        urlPatterns = "/admin/*"
+)
 public class AdminCheckFilter extends HttpFilter {
     @Override
     protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
         //todo#11 /admin/ 하위 요청은 관리자 권한의 사용자만 접근할 수 있습니다. ROLE_USER가 접근하면 403 Forbidden 에러처리
+        HttpSession session = req.getSession(false);
+
+        User user = (User) session.getAttribute("user");
+        if(user.getUserAuth().equals(User.Auth.ROLE_USER)) {
+            res.sendError(HttpServletResponse.SC_FORBIDDEN);
+        }
+
+        chain.doFilter(req, res);
     }
 }
--- src/main/java/com/nhnacademy/shoppingmall/common/filter/WelcomePageFilter.java
+++ src/main/java/com/nhnacademy/shoppingmall/common/filter/WelcomePageFilter.java
@@ -12,11 +12,19 @@ import java.io.IOException;
 
 @Slf4j
 
+@WebFilter(
+        filterName = "welcomePageFilter",
+        urlPatterns = "/"
+)
 public class WelcomePageFilter extends HttpFilter {
 
     @Override
     protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
         //todo#9 /요청이 오면 welcome page인 index.do redirect 합니다.
-
+        if(req.getServletPath().equals("/")) {
+            res.sendRedirect("/index.do");
+            return;
+        }
+        chain.doFilter(req, res);
     }
 }
