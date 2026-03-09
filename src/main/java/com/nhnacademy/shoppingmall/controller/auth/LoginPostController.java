4e8b8a68ad081e02d28eb4a775ba1d1e72859fc7
konayeong <dnjfrhkd402@naver.com>
2026-03-09 17:25:00 +0900

    TODO13 Controller

--- src/main/java/com/nhnacademy/shoppingmall/controller/auth/LoginPostController.java
+++ src/main/java/com/nhnacademy/shoppingmall/controller/auth/LoginPostController.java
@@ -2,12 +2,16 @@ package com.nhnacademy.shoppingmall.controller.auth;
 
 import com.nhnacademy.shoppingmall.common.mvc.annotation.RequestMapping;
 import com.nhnacademy.shoppingmall.common.mvc.controller.BaseController;
+import com.nhnacademy.shoppingmall.user.domain.User;
 import com.nhnacademy.shoppingmall.user.repository.impl.UserRepositoryImpl;
 import com.nhnacademy.shoppingmall.user.service.UserService;
 import com.nhnacademy.shoppingmall.user.service.impl.UserServiceImpl;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.util.Objects;
 
 @RequestMapping(method = RequestMapping.Method.POST,value = "/loginAction.do")
 public class LoginPostController implements BaseController {
@@ -18,6 +22,21 @@ public class LoginPostController implements BaseController {
     public String execute(HttpServletRequest req, HttpServletResponse resp) {
         //todo#13-2 로그인 구현, session은 60분동안 유지됩니다.
 
-        return "shop/main/index";
+        String userId = req.getParameter("user_id");
+        String userPwd = req.getParameter("user_password");
+
+        User user = userService.doLogin(userId, userPwd);
+
+        if(Objects.nonNull(user)) {
+            HttpSession session = req.getSession(true);
+
+            session.setMaxInactiveInterval(60 * 60);
+            session.setAttribute("loginUser", user);
+
+            return "shop/main/index";
+        }else{
+            return "shop/login/login_form"; // 로그인 실패
+        }
+
     }
 }
--- src/main/java/com/nhnacademy/shoppingmall/controller/auth/LogoutController.java
+++ src/main/java/com/nhnacademy/shoppingmall/controller/auth/LogoutController.java
@@ -1,5 +1,23 @@
 package com.nhnacademy.shoppingmall.controller.auth;
 
-public class LogoutController {
+import com.nhnacademy.shoppingmall.common.mvc.annotation.RequestMapping;
+import com.nhnacademy.shoppingmall.common.mvc.controller.BaseController;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.util.Objects;
+
+@RequestMapping(method = RequestMapping.Method.POST, value = {"/logout.do"})
+public class LogoutController implements BaseController {
     //todo#13-3 로그아웃 구현
+    @Override
+    public String execute(HttpServletRequest req, HttpServletResponse resp) {
+        HttpSession session = req.getSession(false);
+
+        if(Objects.nonNull(session)) {
+            session.invalidate();
+        }
+        return "redirect:/login.do";
+    }
 }
